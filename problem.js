c2cf2de63dc3c7b780fa64bfe25775e555e26e8a
Hosain <cryptichasan925@gmail.com>
2025-08-14 01:09:08 +0600

    problem 3

--- problem.js
+++ problem.js
@@ -27,3 +27,14 @@ const books = [
 const bookTitles = books.map(book => book.title);
 
 // console.log(bookTitles); 
+
+// 3.Task: Function Composition
+// Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+const square = x => x * x;
+const double = x => x * 2;
+const addFive = x => x + 5;
+
+// Function composition
+const composedFunction = x => addFive(double(square(x)));
+
+// console.log(composedFunction(3)); 
