ddad2c48bea14a94c82fb0ed56f52d089624d08a
Hosain <cryptichasan925@gmail.com>
2025-08-14 01:07:27 +0600

    problem 2

--- problem.js
+++ problem.js
@@ -13,3 +13,17 @@ const maleNames = people
   .map(person => person.name);
 
 // console.log(maleNames);
+
+
+// 2.Task: Object Manipulation
+// Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+const books = [
+  { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+];
+
+const bookTitles = books.map(book => book.title);
+
+// console.log(bookTitles); 
